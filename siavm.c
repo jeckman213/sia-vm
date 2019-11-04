878851f26f6dd74742d999f8ed01e283964615a3
jeckman21 <36603515+jeckman21@users.noreply.github.com>
2019-11-03 19:59:21 -0500

    Intization of project

---
+++ siavm.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+// Takes in a file name 
+// Read the file into vm's memory
+// .bin file from assembler
+void load(char* filename)
+{
+
+}
+
+// Read instruction bytes from memory
+void fetch()
+{
+
+}
+
+// Read only from the array of bytes from fetch
+// Populate the OP1 and Op2 registers if appropriate
+// Or fetch additional memory needed to complete
+void decode()
+{
+
+}
+
+// Switch statment that 'does the work'
+// Stores the work into result
+void execute()
+{
+
+}
+
+// Write result into final resigster or memory address
+void store()
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    printf("%s", argv[1]);
+    
+    return 1;
+}
